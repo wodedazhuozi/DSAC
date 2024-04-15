9eaddcd78d3d5c57f3736d2ec8a557e013455168
wodedazhuozi <54254123+wodedazhuozi@users.noreply.github.com>
2024-04-15 18:32:10 +0800

    Add files via upload

---
+++ utils/augment_util.py
@@ -0,0 +1,28 @@
+from torch.utils.data import Dataset
+
+class AugmentedDataset(Dataset):
+    def __init__(self, dataset):
+        super(AugmentedDataset, self).__init__()
+        transform = dataset.transform
+        dataset.transform = None
+        self.dataset = dataset
+
+        if isinstance(transform, dict):
+            self.image_transform = transform['standard']
+            self.augmentation_transform = transform['augment']
+
+        else:
+            self.image_transform = transform
+            self.augmentation_transform = transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        sample = self.dataset.__getitem__(index)
+        image = sample['image']
+
+        sample['image'] = self.image_transform(image)
+        sample['image_augmented'] = self.augmentation_transform(image)
+
+        return sample
---
+++ utils/loss.py
@@ -0,0 +1,96 @@
+import os
+import torch
+import numpy as np
+import torch.nn as nn
+import errno
+
+def mkdir_if_missing(directory):
+    if not os.path.exists(directory):
+        try:
+            os.makedirs(directory)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+
+class AverageMeter(object):
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+
+class SimCLRLoss(nn.Module):
+    # Based on the implementation of SupContrast
+    def __init__(self, temperature):
+        super(SimCLRLoss, self).__init__()
+        self.temperature = temperature
+
+    def forward(self, features):
+        """
+        input:
+            - features: hidden feature representation of shape [b, 2, dim]
+
+        output:
+            - loss: loss computed according to SimCLR
+        """
+
+        b, n, dim = features.size()
+        assert (n == 2)
+        mask = torch.eye(b, dtype=torch.float32).cuda()
+
+        contrast_features = torch.cat(torch.unbind(features, dim=1), dim=0)
+        anchor = features[:, 0]
+
+        # Dot product
+        dot_product = torch.matmul(anchor, contrast_features.T) / self.temperature
+
+        # Log-sum trick for numerical stability
+        logits_max, _ = torch.max(dot_product, dim=1, keepdim=True)
+        logits = dot_product - logits_max.detach()
+
+        mask = mask.repeat(1, 2)
+        logits_mask = torch.scatter(torch.ones_like(mask), 1, torch.arange(b).view(-1, 1).cuda(), 0)
+        mask = mask * logits_mask
+
+        # Log-softmax
+        exp_logits = torch.exp(logits) * logits_mask
+        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+
+        # Mean log-likelihood for positive
+        loss = - ((mask * log_prob).sum(1) / mask.sum(1)).mean()
+
+        return loss
